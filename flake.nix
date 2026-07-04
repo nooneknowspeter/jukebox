fd68ecc6c1aa97c7898d466838d5bd59ca8af157
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-04 18:30:17 -0400

    inital commit

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "themes inspired by music cover art";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        runtime_pkgs = with pkgs; [
+          uv
+        ];
+
+        dev_pkgs = with pkgs; [
+          act
+					direnv
+					git
+          just
+        ];
+
+        fmt_pkgs = with pkgs; [
+          treefmt
+
+          nixfmt
+          prettier
+          ruff
+        ];
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+            ]
+            ++ runtime_pkgs
+            ++ dev_pkgs
+            ++ fmt_pkgs;
+          };
+        };
+      }
+    );
+}
