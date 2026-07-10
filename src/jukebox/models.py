4950d5e5941a2e74a0e9db884d978f83102d9090
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2026-07-10 00:25:00 -0400

    refactor: add base key constant

--- src/jukebox/models.py
+++ src/jukebox/models.py
@@ -13,6 +13,25 @@ import regex
 
 HEX_COLOR_RE = regex.compile(r"^#[0-9a-fA-F]{6}$")
 
+PALETTE_BASE_KEYS = [
+    "base00",
+    "base01",
+    "base02",
+    "base03",
+    "base04",
+    "base05",
+    "base06",
+    "base07",
+    "base08",
+    "base09",
+    "base0A",
+    "base0B",
+    "base0C",
+    "base0D",
+    "base0E",
+    "base0F",
+]
+
 
 @dataclass
 class Author:
